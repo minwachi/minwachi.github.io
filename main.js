684f76d19575f915d601307ff193b46c8903e9e6
minwachi <60230414+minwachi@users.noreply.github.com>
2021-06-11 22:59:45 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+function clickBtn1(){
+	const p1 = document.getElementById("form-area1");
+	const p2 = document.getElementById("mainbutton1");
+
+	if(p1.style.display=="block"){
+		// noneで非表示
+		p1.style.display ="none";
+	}else{
+		// blockで表示
+		p1.style.display ="block";
+		p2.style.display ="none";
+	}
+}
+
+function clickBtn2(){
+	const p1 = document.getElementById("form-area2");
+	const p2 = document.getElementById("mainbutton2");
+
+	if(p1.style.display=="block"){
+		// noneで非表示
+		p1.style.display ="none";
+	}else{
+		// blockで表示
+		p1.style.display ="block";
+		p2.style.display ="none";
+	}
+}
+
+function clickBtn3(){
+	const p1 = document.getElementById("form-area3");
+	const p2 = document.getElementById("mainbutton3");
+
+	if(p1.style.display=="block"){
+		// noneで非表示
+		p1.style.display ="none";
+	}else{
+		// blockで表示
+		p1.style.display ="block";
+		p2.style.display ="none";
+	}
+}
